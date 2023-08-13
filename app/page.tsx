5f6a0cd412a23290e223df1d0c554e74029dee3f
chasechristensen1 <chase.christensen@inmoment.com>
2023-08-13 14:42:41 -0600

    slogan gen

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function SloganGenerator() {
 
   return (
     <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
+      <h1 className="text-2xl">Slogan generator</h1>
       <form onSubmit={handleSubmit}>
         <input
           className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2 bg-transparent"
