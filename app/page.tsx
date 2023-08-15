39a7c7d5c3ebd11d34940a9937eb37d80bcbc034
chasechristensen1 <chase.christensen@inmoment.com>
2023-08-14 22:02:02 -0600

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function SloganGenerator() {
           <div className="fixed bottom-8">
             <div className="flex flex-col gap-6 w-full">
               <input
-                className="w-[30rem] max-w-full border border-gray-300 rounded shadow-xl p-2 bg-white dark:bg-gray-800"
+                className="w-[30rem] max-w-full border border-gray-300 rounded-xl shadow-xl px-4 py-2 bg-white dark:bg-gray-800"
                 value={input}
                 placeholder="Describe your business or brand..."
                 onChange={handleInputChange}
